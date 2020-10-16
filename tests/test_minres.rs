6dff641afdd230cb977cc2e96120ad7091619903
Changxi Zheng <cxz@cs.columbia.edu>
2020-10-16 02:06:54 -0400

    -

--- tests/test_minres.rs
+++ tests/test_minres.rs
@@ -62,10 +62,10 @@ fn simple(shape: (usize, usize)) -> (sprs::CsMat<f64>, Vec<f64>) {
     let mut ret_a = sprs::TriMat::<f64>::new((n, n));
     let mut rhs = vec![0_f64; n];
     for (i, v) in rhs.iter_mut().enumerate() {
-        *v = (i+1) as f64;
+        *v = (i + 1) as f64;
     }
     for i in 0..n {
-        ret_a.add_triplet(i, i, ((i + 1)*2) as f64);
+        ret_a.add_triplet(i, i, ((i + 1) * 2) as f64);
     }
     (ret_a.to_csr(), rhs)
 }
@@ -76,42 +76,42 @@ fn grid_laplacian(shape: (usize, usize)) -> (sprs::CsMat<f64>, Vec<f64>) {
     let mut rhs = vec![0_f64; n];
     let mut ret_a = sprs::TriMat::<f64>::new((n, n));
 
-    let bv = |row: isize, col: isize| { (row + col) as f64 };
+    let bv = |row: isize, col: isize| (row + col) as f64;
 
     for i in 0..rows {
         for j in 0..cols {
-            let vid = i*cols + j;
+            let vid = i * cols + j;
 
             ret_a.add_triplet(vid, vid, -4.);
 
             if i > 0 {
-                let tid = (i-1)*cols + j;
+                let tid = (i - 1) * cols + j;
                 ret_a.add_triplet(vid, tid, 1.);
             } else {
-                rhs[vid] -= bv(i as isize-1, j as isize);
+                rhs[vid] -= bv(i as isize - 1, j as isize);
             }
 
             if j > 0 {
-                let tid = i*cols + j-1;
+                let tid = i * cols + j - 1;
                 ret_a.add_triplet(vid, tid, 1.);
             } else {
                 rhs[vid] -= bv(i as isize, j as isize - 1);
             }
 
             if i < rows - 1 {
-                let tid = (i+1) * cols + j;
+                let tid = (i + 1) * cols + j;
                 ret_a.add_triplet(vid, tid, 1.);
             } else {
-                rhs[vid] -= bv(i as isize +1, j as isize);
+                rhs[vid] -= bv(i as isize + 1, j as isize);
             }
 
             if j < cols - 1 {
                 let tid = i * cols + j + 1;
                 ret_a.add_triplet(vid, tid, 1.);
             } else {
-                rhs[vid] -= bv(i as isize, j as isize+1);
+                rhs[vid] -= bv(i as isize, j as isize + 1);
             }
         } // end for
-    } // end for 
+    } // end for
     (ret_a.to_csr(), rhs)
 }
